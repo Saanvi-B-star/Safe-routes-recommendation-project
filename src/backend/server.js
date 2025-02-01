759151a4a8208dffbb3ded131dbef284fbf7fd98
Gunjan925 <innanigunjan@gmail.com>
2025-02-02 01:55:30 +0530

    Changing localhost port in server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -63,6 +63,6 @@ app.post("/login", async (request, response) => {
     }
 });
 
-app.listen(5001, () => {
-    console.log("Server running on port 5001");
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
 });
